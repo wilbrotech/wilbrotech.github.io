01b2fe055f57bd6c3af885e3b8c6f9ef302e72c8
William-Amon <51389306+William-Amon@users.noreply.github.com>
2019-06-18 23:38:46 +0000

    added php file

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'rooticaamon@gmail.com';
+
+    $email_subject = 'New Form submission';
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "User Message: $message.\n".;
+
+    $to = 'amondjani@gmail.com';
+
+    $headers = "From: $email_form \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html")
+?>
